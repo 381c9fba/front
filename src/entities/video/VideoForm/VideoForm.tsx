bcce54bc1a9c5d6f4196de9c1763fed549b3e3c1
Anton Zhuravlev <abramoff.job@gmail.com>
2024-09-27 19:27:45 +0300

    Базовый layout

--- src/entities/video/VideoForm/VideoForm.tsx
+++ src/entities/video/VideoForm/VideoForm.tsx
@@ -4,51 +4,82 @@ import { useRef, useState } from 'react';
 export const VideoForm = () => {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [file, setFile] = useState<File | null>(null);
+
     const handleClick = () => {
         if (fileInputRef.current) {
             fileInputRef.current.click();
         }
     };
 
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    };
+
+    // Drag and drop handlers
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        e.currentTarget.classList.add(cls.dragOver);
+    };
+
+    const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        e.currentTarget.classList.remove(cls.dragOver);
+    };
+
+    const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        e.currentTarget.classList.remove(cls.dragOver);
+
+        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            setFile(e.dataTransfer.files[0]);
+        }
+    };
+
     return (
         <div className={cls.wrapper}>
-            {
-                file
-                    ?
-                    <div className={cls.video}>
-                        <video src={URL.createObjectURL(file)}></video>
-                        <p className={cls.text}>{file.name}</p>
-                        <button
-                            className={cls.submitButton}
-                            type="button"
-                        >Отправить
-                        </button>
-                    </div>
-                    :
-                    <form className={cls.form}>
-                        <div className={cls.fileWrapper}>
-                            <input
-                                onChange={(e) => {
-                                    if (e.target.files) {
-                                        setFile(e.target.files[0]);
-                                    }
-                                }}
-                                ref={fileInputRef} type="file" accept="video/*" />
-                            <div className={cls.info}>
-                                <button
-                                    type="button"
-                                    onClick={handleClick}
-                                >
-                                    Выбрать видео
-                                </button>
-                            </div>
-                            <input
-                                className={cls.urlInput}
-                                placeholder="URL"
-                                type="text" />
+            {file ? (
+                <div className={cls.video}>
+                    <video src={URL.createObjectURL(file)} controls></video>
+                    <p className={cls.text}>{file.name}</p>
+                    <button className={cls.submitButton} type="button">
+                        Отправить
+                    </button>
+                </div>
+            ) : (
+                <form className={cls.form}>
+                    <div className={cls.fileWrapper}>
+                        <input
+                            ref={fileInputRef}
+                            type="file"
+                            accept="video/*"
+                            onChange={handleFileChange}
+                        />
+                        <div
+                            className={cls.info}
+                            onClick={handleClick}
+                            onDragOver={handleDragOver}
+                            onDragLeave={handleDragLeave}
+                            onDrop={handleDrop}
+                        >
+                            <button type="button">Выбрать видео</button>
+                            <p className={cls.text}>
+                                Выберите файлы
+                                или перетащите их сюда
+                            </p>
                         </div>
-                    </form>
-            }
+                        <input
+                            className={cls.urlInput}
+                            placeholder="URL"
+                            type="text"
+                        />
+                    </div>
+                </form>
+            )}
         </div>
     );
 };
